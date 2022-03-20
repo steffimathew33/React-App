ea042071769c7532687f74e66916c89c8dfc7ca8
iTakedown27 <ezrabridger27@gmail.com>
2022-03-20 13:21:37 -0700

    added directories

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const expressLayouts = require('express-ejs-layouts');
+const indexRouter = require('./routes/index');
 
+app.use('view engine', 'ejs');
+app.use(express.static('public'));
+app.use('views', __dirname + '/views');
+app.use('layouts', 'layours/layout');
+app.use(expressLayouts);
+
+app.use('/', indexRouter);
+
+app.get('/', (req, res) => {
+    res.render();
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
